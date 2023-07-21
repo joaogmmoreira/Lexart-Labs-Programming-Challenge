b4af3b82650dd83d8db9fe2c1d35389f7e7daa5b
João Moreira <jgmoreira1809@gmail.com>
2023-07-21 13:51:32 -0300

    Update Programming_Challenge.js

--- Programming_Challenge.js
+++ Programming_Challenge.js
@@ -1,10 +1,4 @@
 /* eslint-disable no-console */
-// Create an algorithm that traverses a one-dimensional
-// array containing letters and numbers: ["a", 10, "b", "hello", 122, 15].
-
-// Obtain an array containing only letters.
-// Obtain an array containing only numbers.
-
 const whatImLookingFor = (type, array) => {
   if (type === 'letters') {
     if (array.some((element) => typeof element === 'string')) {
@@ -37,8 +31,6 @@ const whatImLookingFor = (type, array) => {
 
   return 'None of these elements are numbers';
 };
-
-// Obtain the largest number from the previous array.
 
 const largestNumber = (array) => {
   const numbers = whatImLookingFor('numbers', array);
